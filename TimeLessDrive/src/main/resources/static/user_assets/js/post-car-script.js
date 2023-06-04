bdaceb4d1b6fa4920b6a1b20fe491b86c824fc97
unknown <tridmse173029@fpt.edu.vn>
2023-06-04 16:20:47 +0700

    Update post car form fields

--- TimeLessDrive/src/main/resources/static/user_assets/js/post-car-script.js
+++ TimeLessDrive/src/main/resources/static/user_assets/js/post-car-script.js
@@ -1,19 +1,10 @@
-// Check phone input
-var phoneInput = document.getElementById('phone-number');
-var myForm = document.forms.myForm;
-var result = document.getElementById('phone-number-result');  // only for debugging purposes
-
-phoneInput.addEventListener('input', function (e) {
-    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,4})(\d{0,3})(\d{0,3})/);
-    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-});
-
-myForm.addEventListener('submit', function (e) {
-    phoneInput.value = phoneInput.value.replace(/\D/g, '');
-    result.innerText = phoneInput.value;  // only for debugging purposes
-
-    e.preventDefault(); // You wouldn't prevent it
-});
+// Mask license plate
+// var licensePlate = document.getElementById('license-plate');
+//
+// licensePlate.addEventListener('input', function (e) {
+//     var x = e.target.value.replace(/[a-z]/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})/);
+//     e.target.value = !x[2] ? x[1] :  x[1] + '-' + x[2] + (x[3] ? '.' + x[3] : '');
+// });
 
 // Preview images
 const MAX_IMAGE_NUM = 4;
@@ -40,39 +31,17 @@ function previewImages() {
         }
     }
 }
-// Remove image
-function removeImage(button) {
-    const preview = button.previousElementSibling; // get the <img> element preceding the input element
-    const file = document.getElementById('car-image').files;
-
-    preview.src = ""; // clear the src attribute of the <img> element
-    preview.style.display = 'none';
-    button.style.display = 'none';
-
-    var fileBuffer = new DataTransfer();
-    const index = parseInt(button.id);
-
-    // append the file list to an array iteratively
-    for (let i = 0; i < file.length; i++) {
-        // Exclude file in specified index
-        if (index !== i)
-            fileBuffer.items.add(file[i]);
-    }
-
-    // Assign buffer to file input
-    document.getElementById("car-image").files = fileBuffer.files; // <-- according to your file input reference
-}
 // Check num of files
-$(function () {
-    $("input[type='file']").change(function () {
-        var $fileUpload = $("input[type='file']");
-        if (parseInt($fileUpload.get(0).files.length) > MAX_IMAGE_NUM) {
-            this.value = "";
-            alert("You can only upload a maximum of " + MAX_IMAGE_NUM + " files");
-            previewImages();
-        }
-    });
-});
+// $(function () {
+//     $("input[type='file']").change(function () {
+//         var $fileUpload = $("input[type='file']");
+//         if (parseInt($fileUpload.get(0).files.length) > MAX_IMAGE_NUM) {
+//             this.value = "";
+//             alert("You can only upload a maximum of " + MAX_IMAGE_NUM + " files");
+//             previewImages();
+//         }
+//     });
+// });
 // Check file size
 function uploadField () {
     var uploadField = document.getElementById("car-image");
