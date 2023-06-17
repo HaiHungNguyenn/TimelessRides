b1c04f47b0eaaeaa47120480b5db4b4622151ece
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2023-06-17 23:46:15 +0700

    fixing bug thymeleaf

--- TimeLessDrive/src/main/java/com/duy/carshowroomdemo/config/ThymeleafTemplateConfig.java
+++ TimeLessDrive/src/main/java/com/duy/carshowroomdemo/config/ThymeleafTemplateConfig.java
@@ -1,30 +1,30 @@
-package com.duy.carshowroomdemo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-
-import java.nio.charset.StandardCharsets;
-
-@Configuration
-public class ThymeleafTemplateConfig {
-    @Bean
-    public SpringTemplateEngine springTemplateEngine(){
-        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-        springTemplateEngine.addTemplateResolver(emailTemplateResolver());
-        return springTemplateEngine;
-    }
-
-    private ClassLoaderTemplateResolver emailTemplateResolver() {
-        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
-        emailTemplateResolver.setPrefix("/templates/views/email");
-        emailTemplateResolver.setSuffix(".html");
-        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
-        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        emailTemplateResolver.setCacheable(false);
-        return emailTemplateResolver;
-    }
-}
+//package com.duy.carshowroomdemo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.thymeleaf.spring6.SpringTemplateEngine;
+//import org.thymeleaf.templatemode.TemplateMode;
+//import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+//import org.thymeleaf.templateresolver.ITemplateResolver;
+//
+//import java.nio.charset.StandardCharsets;
+//
+//@Configuration
+//public class ThymeleafTemplateConfig {
+//    @Bean
+//    public SpringTemplateEngine springTemplateEngine(){
+//        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+//        springTemplateEngine.addTemplateResolver(emailTemplateResolver());
+//        return springTemplateEngine;
+//    }
+//
+//    private ClassLoaderTemplateResolver emailTemplateResolver() {
+//        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+////        emailTemplateResolver.setPrefix("/templates/views/email");
+//        emailTemplateResolver.setSuffix(".html");
+//        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+//        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//        emailTemplateResolver.setCacheable(false);
+//        return emailTemplateResolver;
+//    }
+//}
