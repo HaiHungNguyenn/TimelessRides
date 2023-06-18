f6a927e2eed51a3c2d0c4e782b24bfb23f7f5de5
tridominh <tridmse173029@fpt.edu.vn>
2023-06-18 17:20:12 +0700

    Change login front end

--- TimeLessDrive/src/main/resources/static/user_assets/js/login-script.js
+++ TimeLessDrive/src/main/resources/static/user_assets/js/login-script.js
@@ -40,21 +40,32 @@ var check = function() {
 let x;
 const duration = 60;
 let timeRemain = duration;
+const popup1 = ".pop-ups .pop-up:nth-of-type(1)";
+const popup1_esc = ".pop-ups .pop-up:nth-of-type(1) .esc-button";
+const popup2_esc = ".pop-ups .pop-up:nth-of-type(2) .esc-button";
+const popup2 = ".pop-ups .pop-up-active:nth-of-type(2)";
 
 document.querySelector("#forgot-password-button").addEventListener("click", function () {
-    document.querySelector(".pop-up").className = "pop-up-active";
+    document.querySelector(popup1).className = "pop-up-active";
     document.querySelector(".all-page").classList.add("blur");
     x = setInterval(countDown, 1000);
     var  linkToController = "/hello";
     window.location.href=linkToController;
 });
 
-document.querySelector(".pop-up .esc-button").addEventListener("click", function () {
-    document.querySelector(".pop-up-active").className = "pop-up";
+//add event
+
+document.querySelector(popup1_esc).addEventListener("click", function () {
+    document.querySelector(popup1).className = "pop-up";
     document.querySelector(".all-page").classList.remove("blur");
     document.getElementById("countdown-reset-password").innerHTML = duration;
     clearInterval(x);
     timeRemain = duration;
+});
+
+document.querySelector(popup2_esc).addEventListener("click", function () {
+    document.querySelector(popup2).className = "pop-up";
+    document.querySelector(".all-page").classList.remove("blur");
 });
 
 function countDown() {
@@ -79,7 +90,7 @@ function showMessage() {
     })
 }
 
-document.querySelector("#resent-email").addEventListener("click", async function () {
+document.querySelector("#resent-email").addEventListener("click",  function () {
     clearInterval(x);
     timeRemain = duration;
     showMessage();
