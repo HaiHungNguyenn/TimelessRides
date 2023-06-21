9ef14ffd3ea8043a4bd6bbdfc5a396cc66f64f9a
duykasama <duykasama@gmail.com>
2023-06-21 23:07:25 +0700

    add notification to other pages of user interface

--- TimeLessDrive/src/main/resources/static/user_assets/js/account-script.js
+++ TimeLessDrive/src/main/resources/static/user_assets/js/account-script.js
@@ -5,66 +5,72 @@ document.querySelector("#change-password-button").addEventListener("click",funct
         document.getElementsByClassName("change-password active")[0].classList.remove("active");
 });
 
-function updateData(){
-    let url = '/check-notification?id=' + id;
-    let notification = $("#notify-container");
-    $.get(url, function (response){
-        if(response.length > 0){
-            notification.empty();
-            let arrow = document.createElement("div");
-            let notifyContainer = document.getElementById("notify-container");
-            arrow.classList.add("notify-content");
-            arrow.classList.add("my-arrow");
-            notifyContainer.appendChild(arrow);
-            for (let i = 0; i < response.length; i++) {
-                let notifyContent = document.createElement("div");
-                notifyContent.classList.add("notify-content");
-                let span = document.createElement("span");
-                span.innerText = response[i].content;
-                let time = document.createElement("div");
-                time.classList.add("content-end");
-                const [hours, minutes, seconds] = response[i].createTime.split(':');
-                let date = new Date(response[i].createDate).setHours(hours, minutes, seconds);
-                let now = new Date();
-                const timeDiff = Math.abs(now - date);
-                const hour = Math.floor(timeDiff / (1000 * 60 * 60));
-                const minute = Math.floor((timeDiff / (1000 * 60)) % 60);
-                const second = Math.floor((timeDiff / 1000) % 60);
-                const formattedTime = `${hour}h ${minute}m ${second}s`;
-                time.innerText = formattedTime + " ago";
-                notifyContent.appendChild(span);
-                notifyContent.appendChild(time);
-                notifyContainer.appendChild(notifyContent);
-            }
-        }else {
-            let notifyContainer = document.getElementById("notify-container");
-            let notifyContent = document.createElement("div");
-            $("#notify-container").empty();
-            notifyContent.classList.add("notify-content");
-            notifyContent.classList.add("text-center");
-            let span = document.createElement("span");
-            span.innerText = "You don't have any notifications";
-            notifyContent.appendChild(span);
-            let arrow = document.createElement("div");
-            arrow.classList.add("notify-content");
-            arrow.classList.add("my-arrow");
-            notifyContainer.appendChild(arrow);
-            notifyContainer.appendChild(notifyContent);
-        }
-    }, "json");
-}
-
-$(document).ready(function (){
-    $(".fa-bell").click(function (){
-        let e = document.getElementById("notify-container");
-        if (e.classList.contains("my-style")){
-            e.classList.remove("my-style");
-        }else {
-            e.classList.add("my-style");
-        }
-    });
-    updateData();
-    setInterval(function (){
-        updateData();
-    }, 10000);
-});
+// function updateData(){
+//     let url = '/check-notification?id=' + id;
+//     let notification = $(".Notification_body");
+//     $.get(url, function (response){
+//         if(response.length > 0){
+//             notification.empty();
+//             for (let i = 0; i < response.length; i++) {
+//                 let h5 = document.createElement("h5");
+//                 h5.innerText = response[i].content;
+//                 let p = document.createElement("p");
+//                 const [hours, minutes, seconds] = response[i].createTime.split(':');
+//                 let date = new Date(response[i].createDate).setHours(hours, minutes, seconds);
+//                 let now = new Date();
+//                 const timeDiff = Math.abs(now - date);
+//                 const hour = Math.floor(timeDiff / (1000 * 60 * 60));
+//                 const minute = Math.floor((timeDiff / (1000 * 60)) % 60);
+//                 const second = Math.floor((timeDiff / 1000) % 60);
+//                 const formattedTime = `${hour}h ${minute}m ${second}s`;
+//                 p.innerText = formattedTime + " ago";
+//                 let notifyContent = document.createElement("div");
+//                 notifyContent.classList.add("notify_content");
+//                 notifyContent.appendChild(h5);
+//                 notifyContent.appendChild(p);
+//                 let image = document.createElement("img");
+//                 image.src = "../../admin_assets/img/staf/2.png";
+//                 image.alt = "staff_image";
+//                 let notifyThumb = document.createElement("div");
+//                 notifyThumb.classList.add("notify_thumb");
+//                 notifyThumb.appendChild(image);
+//                 let outerDiv = document.createElement("div");
+//                 outerDiv.classList.add("single_notify");
+//                 outerDiv.classList.add("d-flex");
+//                 outerDiv.classList.add("align-items-center");
+//                 outerDiv.appendChild(notifyThumb);
+//                 outerDiv.appendChild(notifyContent);
+//                 document.getElementsByClassName("Notification_body")[0].appendChild(outerDiv);
+//             }
+//         }else {
+//             // let notifyContainer = document.getElementById("notify-container");
+//             // let notifyContent = document.createElement("div");
+//             // $("#notify-container").empty();
+//             // notifyContent.classList.add("notify-content");
+//             // notifyContent.classList.add("text-center");
+//             // let span = document.createElement("span");
+//             // span.innerText = "You don't have any notifications";
+//             // notifyContent.appendChild(span);
+//             // let arrow = document.createElement("div");
+//             // arrow.classList.add("notify-content");
+//             // arrow.classList.add("my-arrow");
+//             // notifyContainer.appendChild(arrow);
+//             // notifyContainer.appendChild(notifyContent);
+//         }
+//     }, "json");
+// }
+//
+// $(document).ready(function (){
+//     // $(".fa-bell").click(function (){
+//     //     let e = document.getElementById("notify-container");
+//     //     if (e.classList.contains("my-style")){
+//     //         e.classList.remove("my-style");
+//     //     }else {
+//     //         e.classList.add("my-style");
+//     //     }
+//     // });
+//     updateData();
+//     setInterval(function (){
+//         updateData();
+//     }, 10000);
+// });
---
+++ TimeLessDrive/src/main/resources/static/user_assets/js/notification-js.js
@@ -0,0 +1,69 @@
+function updateData(){
+    let url = '/check-notification?id=' + id;
+    let notification = $(".Notification_body");
+    $.get(url, function (response){
+        if(response.length > 0){
+            notification.empty();
+            for (let i = 0; i < response.length; i++) {
+                let h5 = document.createElement("h5");
+                h5.innerText = response[i].content;
+                let p = document.createElement("p");
+                const [hours, minutes, seconds] = response[i].createTime.split(':');
+                let date = new Date(response[i].createDate).setHours(hours, minutes, seconds);
+                let now = new Date();
+                const timeDiff = Math.abs(now - date);
+                const hour = Math.floor(timeDiff / (1000 * 60 * 60));
+                const minute = Math.floor((timeDiff / (1000 * 60)) % 60);
+                const second = Math.floor((timeDiff / 1000) % 60);
+                const formattedTime = `${hour}h ${minute}m ${second}s`;
+                p.innerText = formattedTime + " ago";
+                let notifyContent = document.createElement("div");
+                notifyContent.classList.add("notify_content");
+                notifyContent.appendChild(h5);
+                notifyContent.appendChild(p);
+                let image = document.createElement("img");
+                image.src = "../../admin_assets/img/staf/2.png";
+                image.alt = "staff_image";
+                let notifyThumb = document.createElement("div");
+                notifyThumb.classList.add("notify_thumb");
+                notifyThumb.appendChild(image);
+                let outerDiv = document.createElement("div");
+                outerDiv.classList.add("single_notify");
+                outerDiv.classList.add("d-flex");
+                outerDiv.classList.add("align-items-center");
+                outerDiv.appendChild(notifyThumb);
+                outerDiv.appendChild(notifyContent);
+                document.getElementsByClassName("Notification_body")[0].appendChild(outerDiv);
+            }
+        }else {
+            // let notifyContainer = document.getElementById("notify-container");
+            // let notifyContent = document.createElement("div");
+            // $("#notify-container").empty();
+            // notifyContent.classList.add("notify-content");
+            // notifyContent.classList.add("text-center");
+            // let span = document.createElement("span");
+            // span.innerText = "You don't have any notifications";
+            // notifyContent.appendChild(span);
+            // let arrow = document.createElement("div");
+            // arrow.classList.add("notify-content");
+            // arrow.classList.add("my-arrow");
+            // notifyContainer.appendChild(arrow);
+            // notifyContainer.appendChild(notifyContent);
+        }
+    }, "json");
+}
+
+$(document).ready(function (){
+    // $(".fa-bell").click(function (){
+    //     let e = document.getElementById("notify-container");
+    //     if (e.classList.contains("my-style")){
+    //         e.classList.remove("my-style");
+    //     }else {
+    //         e.classList.add("my-style");
+    //     }
+    // });
+    updateData();
+    setInterval(function (){
+        updateData();
+    }, 10000);
+});
