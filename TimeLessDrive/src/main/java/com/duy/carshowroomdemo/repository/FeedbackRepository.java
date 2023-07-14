764b920d97d2696326f978c45fe06e767050bd6c
tridominh <tridmse173029@fpt.edu.vn>
2023-07-14 10:27:09 +0700

    Merge conflict

--- TimeLessDrive/src/main/java/com/duy/carshowroomdemo/repository/FeedbackRepository.java
+++ TimeLessDrive/src/main/java/com/duy/carshowroomdemo/repository/FeedbackRepository.java
@@ -14,14 +14,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, String> {
-<<<<<<< HEAD
 
     @Query("select f from Feedback f where f.rating = :star-0.5 or f.rating = :star order by f.createdAt")
     Page<Feedback> findAllByRating(@Param("star") Double star, Pageable pageable);
-=======
     @Query("select avg(f.rating) from Feedback  f")
     double getAvgStar();
     @Query("select count(f) from Feedback  f")
     int getTotalFb();
->>>>>>> 7f499e8aa629d3fd6713aea288c5cd0709fefb76
 }
